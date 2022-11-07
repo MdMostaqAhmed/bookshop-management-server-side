81deaf0af33b8c216f86a956780301d814978ab2
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-11-07 11:37:08 +0600

    middleware added

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
 
+//Use middleware
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+
 app.get("/", (req, res) => {
     console.log("Server is working")
     res.send("Running bookshop server")
