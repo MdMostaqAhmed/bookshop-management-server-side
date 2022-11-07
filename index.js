2e422fefa92336564b994167521fd3e9dea171fc
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-11-07 11:44:49 +0600

    Add Mongodb Atlas

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //Use middleware
-const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmos1oa.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 app.get("/", (req, res) => {
