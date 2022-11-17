5401acacb2a95d6842bb392e477b8e9dbcba476a
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-11-17 18:15:45 +0600

    JWT implement succsssfully

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    };
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        console.log('decoded:', decoded)
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 //const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmos1oa.mongodb.net/?retryWrites=true&w=majority`;
 // const uri = "mongodb+srv://books:6ItrKtUGil18zL2j@cluster0.xmos1oa.mongodb.net/?retryWrites=true&w=majority";
 //const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -75,14 +92,17 @@ async function run() {
         });
 
         // Load specific product detail by email
-        app.get('/myItem', async (req, res) => {
-            const authHeader = req.headers.authorization;
-            console.log(authHeader);
+        app.get('/myItem', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = bookCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = bookCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            } else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
         });
 
         // Delete item by registered user
